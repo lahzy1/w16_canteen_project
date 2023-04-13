d486a7fd806a511c6f49371f3434614c695cd167
SunEsk <114402024+SunEsk@users.noreply.github.com>
2023-04-13 13:03:16 +0200

    scenebuilder
    added fxid

--- src/main/java/com/example/w16_canteen_project/HelloController.java
+++ src/main/java/com/example/w16_canteen_project/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.w16_canteen_project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label labelDescription, labelPrice, LabelItemInBasket, LabelBalance, labelTotal;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button btnLunch, btnDrinks, btnSandwiches, btnSnacks, btnAddToBasket, btnViewBasket ,btnLogout;
+
+    @FXML
+    private TextField tfSearch;
+
+    @FXML
+    private ImageView ImageviewItem;
 }
